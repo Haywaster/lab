4bd455e66447dc7771ea7f3f309c87c4a3f279a1
Vladimir <haywaster3@gmail.com>
2022-01-16 00:42:28 +0700

    HTML, CSS

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет студентов 5А94</h1>
+            <h2>Общее число студентов: </h2>
+            <h2>Стипендию получат</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
